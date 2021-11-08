b376b277142672bb38fac6f10bdb8a926a174ae6
Erica <ericaspitz522@gmail.com>
2021-11-08 12:11:34 -0700

    Add article ids by splitting short_url and adding to object

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import { fetchTopStories } from './utils/apiCalls'
 const App = () => {
   const [homeStories, setHomeStories] = useState([])
 
+  const setArticleID = (data) => {
+    return data.map(story => {
+      const articleID = story.short_url.split('/')[3]
+      return { ...story, id: articleID }
+    })
+  }
+
   const getTopHomeStories = () => {
     fetchTopStories('home')
-    .then(data => setHomeStories(data.results))
+    .then(data => setHomeStories(setArticleID(data.results)))
   }
 
   useEffect(() => {
