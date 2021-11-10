5c9f71d1f36b92dca19e846dc8d2eba1dc801452
Erica <ericaspitz522@gmail.com>
2021-11-10 09:56:02 -0700

    Edit api call and add check for errors function

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,7 +1,20 @@
-export const fetchTopStories = (section) => {
+export const fetchTopStories = async (section) => {
   const apiKey = '1wyIxNovEbx9RuITWx0ObNyUBuqQuAxP';
-  return fetch(
+  const res = await fetch(
     `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apiKey}`
   )
-  .then(res => res.json())
+  const data = await checkForErrors(res);
+  return data;
+}
+
+export const checkForErrors = (res) => {
+  if (res.status === 404) {
+    throw new Error('Oops, something went wrong. Please check back later.')
+  } else if (res.status === 500) {
+    throw new Error('Server Error. Please check back later.')
+  } else if (res.ok) {
+    return res.json()
+  } else {
+    throw new Error('Oops, something went wrong. Please check back later.')
+  }
 }
