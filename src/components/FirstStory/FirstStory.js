077123b8e9d3748e4eae99d4ba4dd8d7e6043780
Erica <ericaspitz522@gmail.com>
2021-11-12 08:53:00 -0700

    Add basic media queries for tablets and mobile devices

--- src/components/FirstStory/FirstStory.js
+++ src/components/FirstStory/FirstStory.js
@@ -6,19 +6,23 @@ const FirstStory = ({ story }) => {
   const formatID = story.short_url.split('/')
   const id = formatID[formatID.length - 1]
 
+if (story) {
   return (
     <Link to={`/${story.section}/${id}`} className='story-card-link' key={id}>
-      <article className='story-card' id={id}>
-        <div className='card-img-container'>
-          <img className='image' src={story.multimedia[0].url} alt={`${story.title} + image`}/>
-        </div>
-        <div className='title-abstract'>
-          <p className='story-title'>{story.title}</p>
-          <p className='story-abstract'>{story.abstract}</p>
-        </div>
-      </article>
+    <article className='story-card' id={id}>
+    <div className='card-img-container'>
+    <img className='image' src={story.multimedia[0].url} alt={`${story.title} + image`}/>
+    </div>
+    <div className='title-abstract'>
+    <p className='story-title'>{story.title}</p>
+    <p className='story-abstract'>{story.abstract}</p>
+    </div>
+    </article>
     </Link>
   );
+} else {
+  return null
+}
 }
 
 export default FirstStory;
